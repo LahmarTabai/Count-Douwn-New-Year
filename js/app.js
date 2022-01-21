27f5946c4d295fc48ad4de363942cb641eb45d7c
LahmarTabai <79151540+LahmarTabai@users.noreply.github.com>
2022-01-21 11:05:46 +0100

    modification

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const countdown = () => {
-        const countDate = new Date("Jan 1, 2023 00:00:00").getTime();
+        const countDate = new Date("Jan 27, 2022 00:00:00").getTime();
         const now = new Date().getTime();
         const gap = countDate - now;
 
